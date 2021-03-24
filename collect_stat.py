5f9a6dce49fc5f1ec6b8efb208291d3e3d7509c9
bohlinger <patrikb@met.no>
2021-03-24 21:49:52 +0100

    first outcast of collect_stat.py

---
+++ collect_stat.py
@@ -0,0 +1,86 @@
+# --- imports --- #
+
+# standard
+import yaml
+import numpy as np
+from datetime import datetime,timedelta
+import os
+import argparse
+from argparse import RawTextHelpFormatter
+
+# custom
+from stationmod import station_class
+from utils import system_call
+
+# --- parser --- #
+parser = argparse.ArgumentParser(
+    description="""
+Retrieve data from the waverider Tennholmen and dumps to monthly nc-file.
+If file exists, data is appended.
+
+Usage:
+./collect_station.py -sd 2021010100 -ed 2021013123 -stat all
+    """,
+    formatter_class = RawTextHelpFormatter
+    )
+parser.add_argument("-sd", metavar='startdate',
+    help="start date of time period")
+parser.add_argument("-ed", metavar='enddate',
+    help="end date of time period")
+parser.add_argument("-var", metavar='varname',
+    help="variable name"),
+parser.add_argument("-station", metavar='stationname',
+    help="station name")
+parser.add_argument("-sensor", metavar='sensorname',
+    help="sensor name")
+
+args = parser.parse_args()
+
+now = datetime.now()
+if args.sd is None:
+    args.sd = datetime(now.year,now.month,now.day,0)-timedelta(days=1)
+else:
+    args.sd = datetime(int(args.sd[0:4]),int(args.sd[4:6]),
+                int(args.sd[6:8]),int(args.sd[8:10]))
+if args.ed is None:
+    args.ed = datetime(now.year,now.month,now.day)
+else:
+    args.ed = datetime(int(args.ed[0:4]),int(args.ed[4:6]),
+                int(args.ed[6:8]),int(args.ed[8:10]))
+
+if args.var is None:
+    args.var = 'Hs'
+
+print(args)
+
+print( '# Start process of collecting platform' 
+        + ' data and dump to nc-file #')
+
+# --- prerequisites --- #
+
+# find wavy path
+tmppath = str(system_call('echo $PYTHONPATH'))[2:-3]
+wavydir = [s for s in tmppath.split(":") if 'wavy' in s][0]
+
+# get variable info
+configdir = os.path.abspath(os.path.join(wavydir, '..', 'config/station_specs.yaml'))
+with open(configdir,'r') as stream:
+    station_dict=yaml.safe_load(stream)
+
+# settings
+if (args.station is None or args.station == 'all'):
+    platformlst = station_dict['platform'].keys()
+else:
+    platformlst = [args.station]
+
+# --- program body --- #
+for station in platformlst:
+    for sensor in station_dict['platform'][station]['sensor']:
+        print('station:',station,'; with sensor:',sensor)
+        st_obj = station_class(station,sensor,args.sd,args.ed,
+                                   varalias=args.var)
+        # --- write to nc --- #
+        st_obj.write_to_monthly_nc()
+ 
+print( '# Finished process of collecting platform' 
+        + ' data and dump to nc-file #')
